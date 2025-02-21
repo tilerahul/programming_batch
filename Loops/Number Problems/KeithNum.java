7d920585448085b991f04c2053bf476b3d2e1d71
Rahul Tile <tilerahul8068@gmail.com>
2025-02-22 02:29:55 +0530

    keith num and BinaryToDecimal program added

--- Loops/Number Problems/KeithNum.java
+++ Loops/Number Problems/KeithNum.java
@@ -6,19 +6,45 @@ class KeithNum
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter Num : ");
 		int num = sc.nextInt();
-
+		int dup = num;
 		int sum = 0;
-		while(true){
-			int rem;
-			for(int i = num ; i>0; i/=10){
-				rem = i%10;
-				sum += rem;
-			}
-			if(sum>=num)
-				break;
-			else
+		int len = 0;
+		boolean flag = true;
 
-			System.out.println(sum);
+		for(int i= num; i>0; i/=10)
+			len++;
+
+		int [] arr = new int[len];
+
+		for(int i=len-1; i>=0; i--){
+			int rem = num%10;
+			arr[i] = rem;
+			num/=10;
 		}
+
+		for(;;){
+			for(int i=0; i<len; i++)
+				sum += arr[i];
+
+			for(int i=1; i<len; i++)
+				arr[i-1] = arr[i];
+
+			arr[len-1] = sum;
+
+			if(dup == sum)
+				break;
+
+			if(sum>dup){
+				flag = false;
+				break;
+			}
+			sum = 0;
+		}
+
+		if(flag)
+			System.out.println(dup+" is a Keith Number.");
+		else
+			System.out.println(dup+" is not a Keith Number.");
+		
 	}
 }
